051d0ef0d5bd12ea4b7ca0e755dbad4f6cd826aa
Louis Minsky <github@louisminsky.com>
2018-10-17 15:13:12 -0400

    add push notifications

--- script.js
+++ script.js
@@ -10,6 +10,7 @@
   firebase.initializeApp(config);
 
 getLocation();
+Push.Permission.request();
 
 var currentPos;
 var stations;
@@ -48,9 +49,7 @@ function updatePage() {
     var lat2 = geo.coordinates[1];
     var lon2 = geo.coordinates[0];
     var d = distance(lat1, lon1, lat2, lon2);
-    // console.log(lat1, lon1, lat2, lon2);
     if(d < maxDist && (prop.bike_angels_action == "give" || prop.bike_angels_action == "take")) {
-      // console.log(stations[i]);
       angelStations.push({
         index: i,
         id: prop.station_id,
@@ -64,7 +63,6 @@ function updatePage() {
         lat: geo.coordinates[1],
         lon: geo.coordinates[0],
       });
-      // console.log(prop.name, prop.bike_angels_action, prop.bike_angels_points);
     }
   }
 
@@ -75,10 +73,9 @@ function updatePage() {
   angelStations.sort(function(a, b) {
     return b.points - a.points;
   });
-  // console.log(angelStations);
 
   var t = document.getElementById("t1");
-  t.innerHTML = "<tr><th>Points</th><th>Name</th><th>Distance (miles)</th></tr>";
+  t.innerHTML = "<tr><th>Points</th><th>Location</th><th>Distance (miles)</th></tr>";
   for(i in angelStations) {
     var tr = document.createElement("tr");
     var td1 = document.createElement("td");
@@ -89,46 +86,41 @@ function updatePage() {
     } else if(pointDiff < 0) {
       points += "<span class='fewer points'>(" + pointDiff + ")</span>";
     }
-
-//     if(Number(angelStations[i].points) > 2) {
-//       var title = angelStations[i].points;
-//       if(angelStations[i].action == "give") {
-//         title += " (Give)";
-//       } else {
-//         title += " (Take)";
-//       }
-//       const options = {
-//         body: angelStations[i].name
-//       };
-//       registration.showNotification(title, options);
-//     }
-
     td1.innerHTML = points;
     var prevPoints = document.createElement("span");
-    // var td2 = document.createElement("td");
-    // td2.innerHTML = angelStations[i].action;
     var td3 = document.createElement("td");
     var link = "https://www.google.com/maps/place/" + angelStations[i].lat + "+" + angelStations[i].lon;
-    td3.innerHTML = "<a href='" + link + "'>" + angelStations[i].name + "</a>";
+    td3.innerHTML = "<a href='" + link + "' target='_blank'>" + angelStations[i].name + "</a>";
     var td4 = document.createElement("td");
     td4.innerHTML = Math.round(angelStations[i].dist * 100) / 100;
-    // var td5 = document.createElement("td");
-    // td5.innerHTML = angelStations[i].bikes;
-    // var td6 = document.createElement("td");
-    // td6.innerHTML = angelStations[i].docks;
     tr.appendChild(td1);
-    // tr.appendChild(td2);
     tr.appendChild(td3);
     tr.appendChild(td4);
-    // tr.appendChild(td5);
-    // tr.appendChild(td6);
     if(angelStations[i].action == "take") {
       tr.setAttribute("class", "take");
     }
     t.appendChild(tr);
+
+    points = Number(angelStations[i].points);
+    if(i == 0 || points > 1) {
+      var icon = "/assets/give.png";
+      if(angelStations[i].action == "take") {
+        icon = "/assets/take.png";
+      }
+      Push.create(points + " Points", {
+        body: angelStations[i].name,
+        vibrate: [100, 100, 100],    // An array of vibration pulses for mobile devices.
+        icon: icon,
+        requireInteraction: true,
+        tag: angelStations[i].id,
+        onClick: function() {
+            window.open(link);
+        }  
+      });
+    }
   }
-  var t = new Date(lastUpdate);
-  document.getElementById("info").innerHTML = "Last updated: " + t.toLocaleString();
+  var tim = new Date(lastUpdate);
+  document.getElementById("info").innerHTML = "Last updated: " + tim.toLocaleString();
 }
 
 function error(err) {
@@ -142,18 +134,18 @@ function getLocation() {
 }
 
 function distance(lat1, lon1, lat2, lon2, unit) {
-  var radlat1 = Math.PI * lat1/180
-  var radlat2 = Math.PI * lat2/180
-  var theta = lon1-lon2
-  var radtheta = Math.PI * theta/180
+  var radlat1 = Math.PI * lat1/180;
+  var radlat2 = Math.PI * lat2/180;
+  var theta = lon1-lon2;
+  var radtheta = Math.PI * theta/180;
   var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
   if (dist > 1) {
     dist = 1;
   }
-  dist = Math.acos(dist)
-  dist = dist * 180/Math.PI
-  dist = dist * 60 * 1.1515
-  if (unit=="K") { dist = dist * 1.609344 }
-  if (unit=="N") { dist = dist * 0.8684 }
-  return dist
+  dist = Math.acos(dist);
+  dist = dist * 180/Math.PI;
+  dist = dist * 60 * 1.1515;
+  if (unit=="K") { dist = dist * 1.609344; }
+  if (unit=="N") { dist = dist * 0.8684; }
+  return dist;
 }
